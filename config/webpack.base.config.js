f9fb6597cda3231db341100df59a6a023e031de4
sochiuyee <sochiuyee@qq.com>
2023-01-06 14:32:01 +0800

    解析tsx文件

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,28 +1,32 @@
-const path = require('path')
+const path = require('path');
 
 const webpackBaseConfig = {
-  entry: path.join(__dirname, '../src/index.jsx'),
-  output: {
-    path: path.join(__dirname, '../dist'),
-    filename: '[name].[fullhash:4].js'
-  },
-  resolve:{
-    // 导入的模块没有拓展名，就按指定的拓展名一一寻找
-    extensions:['.js','.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js[x]/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.(sc|c)ss/,
-        // 处理sass文件
-        use: ['style-loader','css-loader', 'sass-loader']
-      }
-    ]
-  }
-}
+    entry: path.join(__dirname, '../src/index.jsx'),
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: '[name].[fullhash:4].js',
+    },
+    resolve: {
+        // 导入的模块没有拓展名，就按指定的拓展名一一寻找
+        extensions: ['.ts', '.js', '.jsx', '.tsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js[x]/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.ts[x]/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.(sc|c)ss/,
+                // 处理sass文件
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+};
 
-module.exports = webpackBaseConfig
+module.exports = webpackBaseConfig;
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,21 +1,21 @@
-const path = require('path')
-const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const webpackConfigBase = require('./webpack.base.config')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpackConfigBase = require('./webpack.base.config');
 
 const webpackConfigProd = {
-  mode: 'production',
-  plugins: [
-    new CleanWebpackPlugin(), // 每次打包前清除dist里面的内容
-    new HtmlWebpackPlugin({
-      title: 'React App',
-      filename: 'index.html', // 输出文件的文件名称
-      inject: 'body', // javascript资源将被放置在body元素的底部
-      template: path.join(__dirname, '../src/index.html') // 模板的Webpack相对路径或绝对路径
-    })
-  ]
-}
+    mode: 'production',
+    plugins: [
+        new CleanWebpackPlugin(), // 每次打包前清除dist里面的内容
+        new HtmlWebpackPlugin({
+            title: 'React App',
+            filename: 'index.html', // 输出文件的文件名称
+            inject: 'body', // javascript资源将被放置在body元素的底部
+            template: path.join(__dirname, '../src/index.html'), // 模板的Webpack相对路径或绝对路径
+        }),
+    ],
+};
 
-module.exports = merge(webpackConfigBase, webpackConfigProd)
+module.exports = merge(webpackConfigBase, webpackConfigProd);
